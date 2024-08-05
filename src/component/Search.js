383b7475df00403a05ea8041052e0287a71e80b4
KOWSALYAMURUKESH <v.kowsalyabe@gmail.com>
2024-08-05 13:45:30 +0530

    update project

---
+++ src/component/Search.js
@@ -0,0 +1,9 @@
+function Search()
+{
+    return(
+        <div className="searchbar">
+            <input type="text" placeholder="search"></input>
+        </div>
+    )
+}
+export default Search
--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./style.css"
+import Navbar from "./component/Navbar"
+import About from "./component/About"
+import Footer from "./component/Footer"
+import Search from "./component/Search"
+import Product from "./component/Product"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+
+//navbar component
+
+//Search component
+
+
+//Product Component
+
+    //About component
+   
+
+//footer
+
+
+root.render(<div>
+    <Navbar></Navbar>
+    <Search></Search>
+    <Product></Product>
+    <About></About>
+    <Footer></Footer>
+    </div>)
